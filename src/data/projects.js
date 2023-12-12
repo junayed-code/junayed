a5b4e8ef07c0b1997483c13f2f5907eafb83425d
junayed-code <junayed.code@gmail.com>
2023-12-12 19:03:17 +0600

    Add projects section data

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+import ticTacToeImage from "@/images/tic-tac-toe.png";
+import neotericCalculatorImage from "@/images/neoteric-calculator.png";
+
+const projects = [
+  {
+    name: "Tic Tac Toe Game",
+    image: ticTacToeImage,
+    live: "https://junayed-code.github.io/tic-tac-toe-game",
+    description:
+      "Tic Tac Toe game has a computer player with the minimax algorithm.",
+    technologies: ["HTML", "CSS", "JavaScript", "jQuery", "MiniMax Algorithm"],
+  },
+  {
+    name: "Neoteric Calculator",
+    image: neotericCalculatorImage,
+    live: "https://neoteric-calculator.surge.sh",
+    description: "A beautiful design neoteric calculator app.",
+    technologies: ["HTML", "Tailwind CSS", "JavaScript"],
+  },
+];
+
+export default projects;
