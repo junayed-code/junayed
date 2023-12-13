9808ee801e2a205846817e42d0832e2184b82507
junayed-code <junayed.code@gmail.com>
2023-12-13 09:59:06 +0600

    Create reusable Section composition component

---
+++ src/components/section.jsx
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param {import("react").DetailsHTMLAttributes} param0
+ * @returns {import("react").ReactNode}
+ */
+export default function Section({ className = "", children, ...rest }) {
+  return (
+    <section
+      {...rest}
+      className={"w-full md:max-w-4xl lg:max-w-[1110px] mx-auto py-16 md:py-20 px-5 "
+        .concat(className)
+        .trim()}
+    >
+      {children}
+    </section>
+  );
+}
