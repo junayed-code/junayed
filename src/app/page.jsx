8024bccc0227669274a28147b20e077cd38ecb85
junayed-code <junayed.code@gmail.com>
2023-12-13 11:50:02 +0600

    Create Contact section component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/sections/hero";
 import Projects from "@/components/sections/projects";
 import Experience from "@/components/sections/experience";
+import Contact from "@/components/sections/contact";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Experience />
       <Projects />
+      <Contact />
     </main>
   );
 }
---
+++ src/components/sections/contact.jsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Link from "next/link";
+import Section from "../section";
+import toast from "react-hot-toast";
+
+export default function Contact() {
+  const handleSubmit = e => {
+    e.preventDefault();
+    setTimeout(() => {
+      toast.success("Email sent successfully!", { duration: 3200 });
+      e.target?.reset();
+    }, 1200);
+  };
+
+  return (
+    <Section id="contact">
+      <h2 className="text-3xl md:text-4xl font-bold font-secondary text-center text-slate-100 mb-4">
+        get in touch with me
+      </h2>
+      <p className="max-w-2xl mx-auto sm:text-lg text-center">
+        Have any questions or proposals? Or perhaps you just want to say hello?
+        Go ahead. Or shoot me an email directly to{" "}
+        <Link
+          href="mailto:junayedakbour@gmail.com"
+          className="font-medium text-slate-100"
+        >
+          junayedakbour@gmail.com
+        </Link>
+      </p>
+
+      <div className="mt-10">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-y-5 max-w-2xl mx-auto"
+        >
+          <input
+            className="bg-transparent text-slate-400 outline-none px-4 py-3 border border-slate-600 focus:border-emerald-400 rounded-md"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+            required
+            minLength={3}
+            maxLength={20}
+          />
+          <input
+            className="bg-transparent text-slate-400 outline-none px-4 py-3 border border-slate-600 focus:border-emerald-400 rounded-md"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+          <textarea
+            name="message"
+            id="message"
+            rows="5"
+            placeholder="Write your message"
+            className="bg-transparent text-slate-400 outline-none px-4 py-3 border border-slate-600 focus:border-emerald-400 rounded-md"
+            required
+            minLength={7}
+          ></textarea>
+
+          <button
+            type="submit"
+            className="self-start border border-emerald-400 bg-slate-900 text-slate-100 py-3 px-5 text-lg hover:shadow-[4px_4px_rgb(52,211,153)] hover:-translate-x-1 hover:-translate-y-1 duration-200"
+          >
+            send message
+          </button>
+        </form>
+      </div>
+    </Section>
+  );
+}
