b4548fdadb4b1619b124cf40e8856c6b4dea6531
junayed-code <junayed.code@gmail.com>
2023-12-15 15:15:23 +0600

    Update hero section UI

--- src/components/sections/hero.jsx
+++ src/components/sections/hero.jsx
@@ -1,19 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import profileImage from "@/images/profile.jpg";
-import Section from "../section";
 
 export default function Hero() {
   return (
-    <Section id="hero" className="mt-[74px] flex flex-col md:flex-row gap-y-10">
+    <section
+      id="hero"
+      className="w-full md:max-w-4xl lg:max-w-[1110px] mx-auto py-16 md:py-24 px-5 mt-[74px] flex flex-col md:flex-row gap-y-10"
+    >
       <div className="md:w-1/2">
         <h5 className="text-xl font-semibold font-secondary text-emerald-300">
           Hi, I am
         </h5>
-        <h1 className="text-5xl md:text-6xl font-secondary font-black text-slate-100 mb-3">
+        <h1 className="text-5xl md:text-[54px] lg:text-6xl font-secondary font-black text-slate-100 mb-3">
           junayed akbor
         </h1>
-        <h3 className="text-xl md:text-2xl font-semibold mb-5 text-slate-300">
+        <h3 className="text-xl md:text-2xl font-semibold mb-3 sm:mb-5 text-slate-300">
           Self-Taught Front-End Web Developer
         </h3>
         <p className="sm:text-lg max-w-xl font-medium">
@@ -41,6 +43,6 @@ export default function Hero() {
           className="mx-auto md:mr-0 border-2 border-emerald-400 duration-300 md:hover:shadow-[4px_4px_#34d399] md:hover:-translate-x-1 md:hover:-translate-y-1 cursor-crosshair"
         />
       </div>
-    </Section>
+    </section>
   );
 }
