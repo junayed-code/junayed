5765880ca675c1f0fb0f6a7f6716dfeaf0ee729e
junayed-code <junayed.code@gmail.com>
2023-12-18 12:22:21 +0600

    Add config.js file

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  navItems: [
+    {
+      path: "#experience",
+      text: "Experience",
+    },
+    {
+      path: "#projects",
+      text: "Projects",
+    },
+    {
+      path: "#contact",
+      text: "Contact",
+    },
+  ],
+};
