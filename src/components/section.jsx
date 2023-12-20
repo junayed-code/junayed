3b6b806caa545f0b5ef171f2a81e2f3eb4ff459d
junayed-code <junayed.code@gmail.com>
2023-12-20 11:45:28 +0600

    Add scroll margin style to Section component

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -7,7 +7,7 @@ export default function Section({ className = "", children, ...rest }) {
   return (
     <section
       {...rest}
-      className={"w-full md:max-w-4xl lg:max-w-[1110px] mx-auto py-16 md:py-20 px-5 "
+      className={"w-full md:max-w-4xl lg:max-w-[1110px] mx-auto py-16 md:py-20 px-5 scroll-mt-6 "
         .concat(className)
         .trim()}
     >
