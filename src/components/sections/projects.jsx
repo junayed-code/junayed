5cde3327bcbb3b3b4727ddbe31c37ca37093a72b
junayed-code <junayed.code@gmail.com>
2024-01-30 18:57:48 +0600

    Update projects section to add scroll parallax animation

--- src/components/sections/projects.jsx
+++ src/components/sections/projects.jsx
@@ -1,40 +1,7 @@
+import Section from "@c/section";
 import projects from "@/data/projects";
-import Section from "../section";
-import Image from "next/image";
-import Link from "next/link";
-
-const ProjectCard = ({ project }) => {
-  const { name, image, live, description, technologies } = project;
-
-  return (
-    <Link
-      href={live}
-      target="_blank"
-      className="block w-full max-w-md sm:max-w-xl rounded-lg overflow-hidden shadow-[0px_0px_80px_-25px_rgba(255,255,255,0.2)] group"
-    >
-      <figure className="relative">
-        <Image src={image} alt={name} priority />
-
-        <figcaption className="absolute inset-0 grid content-end bg-slate-800/60 text-slate-100 p-5 sm:translate-y-full sm:group-hover:translate-y-0 duration-300">
-          <h3 className="text-xl sm:text-2xl font-bold font-secondary mb-1">
-            {name}
-          </h3>
-          <p className="text-sm sm:text-base">{description}</p>
-          <div className="mt-3 sm:mt-4 flex gap-2 flex-wrap">
-            {technologies?.map(tech => (
-              <span
-                key={tech}
-                className="px-3 py-1 rounded-3xl bg-emerald-100 text-emerald-600 text-xs sm:text-sm"
-              >
-                {tech}
-              </span>
-            ))}
-          </div>
-        </figcaption>
-      </figure>
-    </Link>
-  );
-};
+import ProjectCard from "@c/project-card";
+import ScrollParallax from "@c/animate/scroll-parallax";
 
 export default function Projects() {
   return (
@@ -47,11 +14,11 @@ export default function Projects() {
         future.
       </p>
 
-      <div className="mt-12 grid lg:grid-cols-2 justify-items-center gap-6">
+      <ScrollParallax>
         {projects.map(project => (
           <ProjectCard key={project.name} project={project} />
         ))}
-      </div>
+      </ScrollParallax>
     </Section>
   );
 }
