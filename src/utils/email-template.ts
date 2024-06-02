5a9632039bccd747cf3b18bfd9a1baec7342b765
junayed-code <junayed.code@gmail.com>
2024-06-02 15:31:26 +0600

    Update the email template

--- src/utils/email-template.ts
+++ src/utils/email-template.ts
@@ -1,13 +1,13 @@
 export const emailTemplate = (name: string, email: string, message: string) => `
-  <h2>
+  <h3>
     This message is sent from 
     <em><a href="mailto:${email}">${email}</a></em>
-  </h2>
+  </h3>
   <div style="font-size:16px;">
     <p>
-      <em><b style="font-size:18px">"</b></em>
+      <em><b style="font-size:20px;">"</b></em>
       ${message}
-      <em><b style="font-size:18px">"</b></em>
+      <em><b style="font-size:20px;">"</b></em>
     </p>
     <p>Thanks,<br/> <em>${name}</em></p>
   </div>
