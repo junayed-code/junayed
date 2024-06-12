005b5c9282689a05b3fcff1fa06c269d3b34fbc4
junayed-code <junayed.code@gmail.com>
2024-06-12 21:23:40 +0600

    Update the contact section component

--- src/components/sections/contact/index.tsx
+++ src/components/sections/contact/index.tsx
@@ -1,28 +1,28 @@
 import Section from "@components/section";
+import Container from "@components/container";
 import ContactForm from "./contact-form";
 
 export default function Contact() {
   return (
     <Section id="contact">
-      <div className="flex flex-col md:flex-row gap-x-14 gap-y-10">
-        <div className="text-center md:text-left md:basis-1/2">
-          <h2 className="text-3xl md:text-4xl font-bold font-secondary text-primary-foreground mb-4">
-            get in touch with me
+      <Container className="flex flex-col gap-x-14 gap-y-10 md:flex-row">
+        <div className="text-center md:basis-1/2 md:text-left">
+          <h2>
+            get in touch <span className="text-primary-500">with me</span>
           </h2>
           <p className="mx-auto md:text-lg">
             Have any questions or proposals? Or perhaps you just want to say
             hello? Go ahead. Or shoot me an email directly to{" "}
             <a
               href="mailto:junayedakbour@gmail.com"
-              className="font-medium text-primary-foreground"
+              className="font-medium text-neutral-100 hover:text-primary-500"
             >
               junayedakbour@gmail.com
             </a>
           </p>
         </div>
-
         <ContactForm />
-      </div>
+      </Container>
     </Section>
   );
 }
