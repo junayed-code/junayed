27c0e72596df1c106ba8fffc47e05050a7166ec2
junayed-code <junayed.code@gmail.com>
2024-06-15 11:56:24 +0600

    Update the experience section
    - Skills data have been included to the src/config.ts file.
    - Wikipedia api is used to get info about skill.

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -1,54 +1,31 @@
+import Skills from "@components/skills";
 import Section from "@components/section";
+import Container from "@components/container";
 
-export default function Experience() {
+import { getInfoFromWikipedia } from "@/lib/wikipedia";
+import config from "@/config";
+
+export default async function Experience() {
+  const skills = await Promise.all(
+    config.skills.map(async skill => {
+      const info = await getInfoFromWikipedia(skill.wikiTitle);
+      return { ...skill, info };
+    }),
+  );
+
   return (
     <Section id="experience">
-      <h2 className="text-3xl sm:text-4xl font-secondary font-bold text-center text-slate-100 mb-4">
-        experience of my dev journey
-      </h2>
-      <p className="max-w-2xl mx-auto sm:text-lg text-center">
-        I have gained skills with various technological tools throughout my
-        journey as a developer. Through these skills, I have gained a
-        competitive edge and the ability to work efficiently.
-      </p>
-
-      {/* Skill icons container */}
-      <div className="mt-10">
-        <div className="max-w-[400px] w-full aspect-square mx-auto rounded-full relative sm:animate-spin animation-duration-20">
-          {/* Middle element */}
-          <div className="absolute cursor-pointer top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl">
-            <img
-              className="w-14 h-14 shadow-[0px_0px_110px_10px_rgba(255,255,255,0.2)] rounded-xl animate-spin animation-duration-6"
-              src="https://skillicons.dev/icons?i=react"
-              alt="React icon"
-            />
-          </div>
-          <div className="absolute cursor-pointer top-0 left-1/2 -translate-x-1/2">
-            <img src="https://skillicons.dev/icons?i=mongodb" />
-          </div>
-          <div className="absolute cursor-pointer bottom-0 left-1/2 -translate-x-1/2">
-            <img src="https://skillicons.dev/icons?i=nodejs" />
-          </div>
-          <div className="absolute cursor-pointer top-[54px] left-[54px]">
-            <img src="https://skillicons.dev/icons?i=js" />
-          </div>
-          <div className="absolute cursor-pointer top-[54px] right-[54px]">
-            <img src="https://skillicons.dev/icons?i=css" />
-          </div>
-          <div className="absolute cursor-pointer bottom-[54px] left-[54px]">
-            <img src="https://skillicons.dev/icons?i=express" />
-          </div>
-          <div className="absolute cursor-pointer bottom-[54px] right-[54px]">
-            <img src="https://skillicons.dev/icons?i=firebase" />
-          </div>
-          <div className="absolute cursor-pointer top-1/2 -translate-y-1/2 left-0">
-            <img src="https://skillicons.dev/icons?i=tailwind" />
-          </div>
-          <div className="absolute cursor-pointer top-1/2 -translate-y-1/2 right-0">
-            <img src="https://skillicons.dev/icons?i=html" />
-          </div>
-        </div>
-      </div>
+      <Container>
+        <h2 className="text-center">
+          <span className="text-primary-500">experience</span> of my dev journey
+        </h2>
+        <p className="mx-auto max-w-2xl text-center sm:text-lg">
+          I have gained skills with various technological tools throughout my
+          journey as a developer. Through these skills, I have gained a
+          competitive edge and the ability to work efficiently.
+        </p>
+        <Skills skills={skills} />
+      </Container>
     </Section>
   );
 }
